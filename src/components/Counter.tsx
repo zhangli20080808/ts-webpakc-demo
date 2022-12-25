0cc3d5a852e64b8de80c6e99af8d61698e6b3a3d
zhangli20080808 <791492434@qq.com>
2022-12-25 22:28:07 +0800

    zl

---
+++ src/components/Counter.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+interface Props {
+  number: number;
+}
+
+export default class Counter extends React.Component<Props> {
+  render() {
+    const { number } = this.props;
+    return (
+      <div>
+        <p>{number}</p>
+      </div>
+    );
+  }
+}
