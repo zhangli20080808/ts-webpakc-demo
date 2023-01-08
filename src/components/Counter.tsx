faa98c4e842ddeb9faec7780dcbf64297997b12b
zhangli20080808 <791492434@qq.com>
2023-01-08 13:14:07 +0800

    l类组件属性定义

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,12 +1,14 @@
-import * as React from 'react';
+import React, { PropsWithChildren } from 'react';
 
+// type PropsWithChildren<P = unknown> = P & { children?: ReactNode | undefined };
 interface Props {
   number: number;
+  // 在 React 18 的 FC 中，不存在 children 属性，需要手动申明
+  children: PropsWithChildren;
 }
-
 export default class Counter extends React.Component<Props> {
   render() {
-    const { number } = this.props;
+    const { number, children } = this.props;
     return (
       <div>
         <p>{number}</p>
@@ -14,4 +16,11 @@ export default class Counter extends React.Component<Props> {
     );
   }
 }
- 
+
+const MyButton: React.FC<PropsWithChildren<{ color: string }>> = ({
+  children,
+  color,
+}) => {
+  // 在 React 18 的 FC 中，不存在 children 属性，需要手动申明
+  return <div>{children}</div>;
+};
